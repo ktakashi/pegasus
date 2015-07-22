1540531942d6c3b31885773651ed872a924aaccc
ktakashi <ktakashi@ymail.com>
2015-07-22 13:05:58 +0200

    Adding R7RS PostgreSQL library

---
+++ formula/postgresql.scm
@@ -0,0 +1,9 @@
+(formula
+ (description "R7RS PostgreSQL")
+ (version "15.07.21")
+ (homepage :url "https://github.com/ktakashi/r7rs-postgresql")
+ (author :name "Takashi Kato" :email "ktakashi@ymail.com")
+ (source 
+  :type tar :compression gzip
+  :url "https://github.com/ktakashi/r7rs-postgresql/archive/15.07.21.tar.gz")
+ (install (directories ("lib"))))
