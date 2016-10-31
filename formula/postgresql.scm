8931b2be517a0923f63cbf6e390add3139bbdfb9
Takashi Kato <ktakashi@ymail.com>
2016-10-31 13:32:10 +0100

    Updating postgresql version

--- formula/postgresql.scm
+++ formula/postgresql.scm
@@ -1,10 +1,10 @@
 (formula
  (description "R7RS PostgreSQL")
- (version "16.03.08")
+ (version "16.10.31")
  (homepage :url "https://github.com/ktakashi/r7rs-postgresql")
  (author :name "Takashi Kato" :email "ktakashi@ymail.com")
  (source 
   :type tar :compression gzip
-  :url "https://github.com/ktakashi/r7rs-postgresql/archive/16.03.08.tar.gz")
+  :url "https://github.com/ktakashi/r7rs-postgresql/archive/16.10.31.tar.gz")
  (install (directories ("lib")))
  (tests (test :file "test.scm" :style srfi-64 :loadpath "lib")))
