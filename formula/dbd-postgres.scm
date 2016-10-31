a449e8769ea547fdc9e5e79b1b6e2645021394b7
Takashi Kato <ktakashi@ymail.com>
2016-10-31 13:38:06 +0100

    Adding DBD PostgreSQL

---
+++ formula/dbd-postgres.scm
@@ -0,0 +1,17 @@
+;; DBD SQLite3
+(formula
+ (description "PostgreSQL DBD for Sagittarius")
+ (version "16.10.31")
+ (homepage :url "https://github.com/ktakashi/sagittarius-dbd-postgres")
+ (dependencies (dependency :name "postgresql" :version "16.10.31"))
+ (author :name "Takashi Kato" :email "ktakashi@ymail.com")
+ (source 
+  ;;:type tar :compression gzip
+  :type zip
+  :url "https://github.com/ktakashi/sagittarius-dbd-postgres/archive/16.10.31.zip")
+
+ (install (directories ("lib"))))
+
+ 
+ 
+ 
