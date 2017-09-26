a3aa43bd9363064fcf3daf0a4186513edd572b82
ktakashi <ktakashi@ymail.com>
2017-09-26 20:28:08 +0200

    Updating version of postgres bindings

--- formula/dbd-postgres.scm
+++ formula/dbd-postgres.scm
@@ -3,7 +3,7 @@
  (description "PostgreSQL DBD for Sagittarius")
  (version "16.10.31")
  (homepage :url "https://github.com/ktakashi/sagittarius-dbd-postgres")
- (dependencies (dependency :name "postgresql" :version "16.10.31"))
+ (dependencies (dependency :name "postgresql" :version "17.09.26"))
  (author :name "Takashi Kato" :email "ktakashi@ymail.com")
  (source 
   ;;:type tar :compression gzip
--- formula/postgresql.scm
+++ formula/postgresql.scm
@@ -1,10 +1,10 @@
 (formula
  (description "R7RS PostgreSQL")
- (version "16.10.31")
+ (version "17.09.26")
  (homepage :url "https://github.com/ktakashi/r7rs-postgresql")
  (author :name "Takashi Kato" :email "ktakashi@ymail.com")
  (source 
   :type tar :compression gzip
-  :url "https://github.com/ktakashi/r7rs-postgresql/archive/16.10.31.tar.gz")
+  :url "https://github.com/ktakashi/r7rs-postgresql/archive/17.09.26.tar.gz")
  (install (directories ("lib")))
  (tests (test :file "test.scm" :style srfi-64 :loadpath "lib")))
