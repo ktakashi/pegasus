7f4af0baeb3abba70cafd7d8418b727140b7652f
Takashi Kato <ktakashi@ymail.com>
2019-04-23 10:09:49 +0200

    Update mongodb binding version

--- formula/mongodb.scm
+++ formula/mongodb.scm
@@ -1,13 +1,12 @@
 (formula
  (description "R6RS MongoDB")
- (version "18.06.13")
+ (version "19.04.23")
  (homepage :url "https://github.com/ktakashi/r6rs-mongodb")
  (author :name "Takashi Kato" :email "ktakashi@ymail.com")
  (source 
   :type tar :compression gzip
-  :url "https://github.com/ktakashi/r6rs-mongodb/archive/18.06.13.tar.gz")
- (install (directories ("src")
-		       ("contrib/authenticate/src")))
+  :url "https://github.com/ktakashi/r6rs-mongodb/archive/19.04.23.tar.gz")
+ (install (directories ("src") ("contrib/authenticate/src")))
  (tests (test :file "test/bson-parser.scm" :style srfi-64 :loadpath ("src" "test/lib"))
 	(test :file "test/bson-validators.scm" :style srfi-64 :loadpath ("src" "test/lib"))
 	(test :file "test/bson-writer.scm" :style srfi-64 :loadpath ("src" "test/lib"))
@@ -17,5 +16,6 @@
 	(test :file "test/iso-date.scm" :style srfi-64 :loadpath ("src" "test/lib"))
 	(test :file "test/mongodb.scm" :style srfi-64 :loadpath ("src" "test/lib"))
 	(test :file "test/protocol.scm" :style srfi-64 :loadpath ("src" "test/lib"))
-	(test :file "test/uuid.scm" :style srfi-64 :loadpath ("src" "test/lib")))
+	(test :file "test/uuid.scm" :style srfi-64 :loadpath ("src" "test/lib"))
+	(test :file "test/session.scm" :style srfi-64 :loadpath ("src" "test/lib")))
  )
